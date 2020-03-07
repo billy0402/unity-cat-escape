ffd7a9db40ef981e81cc9ee042b37956d0253079
Billy Huang <10646003@ntub.edu.tw>
2020-03-07 17:18:01 +0800

    5-7 - Prefab 和工廠的建立方法

---
+++ Assets/Scripts/ArrowGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowGenerator : MonoBehaviour {
+    public GameObject arrowPrefab;
+    private float _span = 1.0f;
+    private float _delta = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        this._delta += Time.deltaTime;
+        if (this._delta > this._span) {
+            this._delta = 0;
+            GameObject go = Instantiate(arrowPrefab) as GameObject;
+            int px = Random.Range(-6, 7);
+            go.transform.position = new Vector3(px, 7, 0);
+        }
+    }
+}
