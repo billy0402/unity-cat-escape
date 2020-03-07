ec5254e1d43595aad1d9a16ae6fa91aa0451975a
Billy Huang <10646003@ntub.edu.tw>
2020-03-07 16:37:51 +0800

    5-4 - 使用按鈕操作遊戲角色

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // 點擊左方按鈕時
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            transform.Translate(-3, 0, 0); // 往左移動 3
+        }
+
+        // 點擊右方按鈕時
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            transform.Translate(3, 0, 0); // 往右移動 3
+        }
+    }
+}
