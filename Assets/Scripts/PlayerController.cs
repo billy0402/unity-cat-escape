d83091004376a143ffc640d2c39b68165ba15608
Billy Huang <10646003@ntub.edu.tw>
2020-03-07 17:57:28 +0800

    5-10 - 在手機上執行遊戲

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,4 +19,12 @@ public class PlayerController : MonoBehaviour {
             transform.Translate(3, 0, 0); // 往右移動 3
         }
     }
+
+    public void LButtonDown() {
+        transform.Translate(-3, 0, 0);
+    }
+
+    public void RButtonDown() {
+        transform.Translate(3, 0, 0);
+    }
 }
